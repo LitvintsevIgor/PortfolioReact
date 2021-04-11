f6c16146d9db67ce72d924a606fd1ade30fb4c26
igorlitvintsev <litvincevi@mail.ru>
2021-04-11 21:23:19 +0300

    added transform on links

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -7,19 +7,27 @@ export const Header = () => {
             <ul className={style.iconMenu}>
                 <li className={style.iconBox}>
                     <i className={style.iconHome}></i>
-                    <a href="#"></a>
+                    <a href="#">
+                        <h2>Home</h2>
+                    </a>
                 </li>
                 <li className={style.iconBox}>
                     <i className={style.iconAbout}></i>
-                    <a href="#"></a>
+                    <a href="#">
+                        <h2>About</h2>
+                    </a>
                 </li>
                 <li className={style.iconBox}>
                     <i className={style.iconPortfolio}></i>
-                    <a href="#"></a>
+                    <a href="#">
+                        <h2>Portfolio</h2>
+                    </a>
                 </li>
                 <li className={style.iconBox}>
                     <i className={style.iconContact}></i>
-                    <a href="#"></a>
+                    <a href="#">
+                        <h2>Contact</h2>
+                    </a>
                 </li>
             </ul>
         </div>
