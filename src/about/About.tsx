000ab7bcced0dea3fa86b719b00ed92296a5ffd0
igorlitvintsev <litvincevi@mail.ru>
2021-05-02 00:15:43 +0300

    start sreate about page

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,10 +1,76 @@
 import React from "react";
 import style from "./About.module.css"
+import photoAva from "./../images/photoAva.jpeg"
 
 export const About = () => {
+
+    // let aboutWrapper = style.about + " " + style.container
+
     return (
         <div className={style.about}>
-            About
+            <div className={style.container}>
+                <div className={style.test}></div>
+                <h1>About me</h1>
+                <div className={style.mainInfo}>
+                    <div className={style.personalInfo}>
+                        <h2>Personal info</h2>
+                        <div className={style.dataAboutMe}>
+                            <div className={style.firstColumn}>
+                                <ul>
+                                    <li>
+                                        <span>First Name:</span>
+                                        <span> Igor</span>
+                                    </li>
+                                    <li>
+                                        <span>Last Name:</span>
+                                        <span> Litvintsev</span>
+                                    </li>
+                                    <li>
+                                        <span>Age:</span>
+                                        <span> 27</span>
+                                    </li>
+                                    <li>
+                                        <span>Nationality:</span>
+                                        <span> Russian</span>
+                                    </li>
+                                    <li>
+                                        <span>Freelance:</span>
+                                        <span> Available</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={style.secondColumn}>
+                                <ul>
+                                    <li>
+                                        <span>Address:</span>
+                                        <span> Voronezh</span>
+                                    </li>
+                                    <li>
+                                        <span>Phone:</span>
+                                        <span> +79515642759</span>
+                                    </li>
+                                    <li>
+                                        <span>Email:</span>
+                                        <span> litvincevi@mail.ru</span>
+                                    </li>
+                                    <li>
+                                        <span>Skype:</span>
+                                        <span> Litvintsev.Igor</span>
+                                    </li>
+                                    <li>
+                                        <span>Langages:</span>
+                                        <span> Russian, English</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.photo}>
+                        <img src={photoAva} alt=""/>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
